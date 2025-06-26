3e7bc5303864a310ec118442280c31f0c4283a3b
edarizam <edarizam@unal.edu.co>
2025-06-26 02:27:17 -0500

    Añade store de carrito de compra

---
+++ frontend/my-app/src/lib/store/shoppingCart.svelte.ts
@@ -0,0 +1,51 @@
+import type { ProductInterface, service } from '$lib/types';
+
+class Cart {
+	products: ProductInterface[] = $state([]);
+	services: service[] = $state([]);
+	client: string | null = $state(null); // Usar client interface en un futuro
+	total: number = $state(0);
+
+	addProduct(product: ProductInterface) {
+		this.products.push(product);
+		this.recalculateTotal();
+	}
+
+	removeProductById(id: number) {
+		this.products = this.products.filter((p) => p.id !== id);
+		this.recalculateTotal();
+	}
+
+	addService(service: service) {
+		this.services.push(service);
+		this.recalculateTotal();
+	}
+
+	removeServiceById(id: number) {
+		this.services = this.services.filter((s) => s.id !== id);
+		this.recalculateTotal();
+	}
+
+	clearCart() {
+		this.products = [];
+		this.services = [];
+		this.client = null;
+		this.total = 0;
+	}
+
+	recalculateTotal() {
+		const totalProductos = this.products.reduce((acc, p) => acc + p.price, 0);
+		const totalServicios = this.services.reduce((acc, s) => acc + s.price, 0);
+		this.total = totalProductos + totalServicios;
+	}
+
+	findProductById(id: number): ProductInterface | null {
+		return this.products.find((p) => p.id === id) || null;
+	}
+
+	findServiceById(id: number): service | null {
+		return this.services.find((s) => s.id === id) || null;
+	}
+}
+
+export const cartStore = new Cart();
