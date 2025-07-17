994e483caf0963491090d72da167dbb7488ff506
edarizam <edarizam@unal.edu.co>
2025-07-17 03:50:16 -0500

    Hotfix lógica de modal

--- frontend/my-app/src/lib/components/product/ProductCardOrdenSummary.svelte
+++ frontend/my-app/src/lib/components/product/ProductCardOrdenSummary.svelte
@@ -69,15 +69,27 @@
 		</Card.Content>
 	</Card.Root>
 
-	{#if product?.quantity! > product?.stock! || product?.quantity! < 0 || !Number.isInteger(product?.quantity) || product?.quantityService! > product?.stock! || product?.quantityService! < 0 || !Number.isInteger(product?.quantityService)}
+	{#if product?.quantity! > product?.stock! || product?.quantity! < 0 || product?.quantity % 1 !== 0}
 		<SuccessOrFailDialog
 			infoDialog={false}
 			contentDialog={`La cantidad ingresada no puede ser procesada ${
-				product?.quantity! > product?.stock! || product?.quantityService! > product?.stock!
-					? ': El stock disponible es insuficiente'
-					: ''
+				product?.quantity! > product?.stock! ? ': El stock disponible es insuficiente' : ''
 			}`}
-			callback={() => (product!.quantity = product?.stock)}
+			callback={() => {
+				product!.quantity = product?.stock;
+			}}
+		/>
+	{/if}
+
+	{#if product?.quantityService! > product?.stock! || product?.quantityService! < 0 || product.quantityService % 1 !== 0}
+		<SuccessOrFailDialog
+			infoDialog={false}
+			contentDialog={`La cantidad ingresada no puede ser procesada ${
+				product?.quantityService! > product?.stock! ? ': El stock disponible es insuficiente' : ''
+			}`}
+			callback={() => {
+				product!.quantityService = product?.stock;
+			}}
 		/>
 	{/if}
 {/if}
